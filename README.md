4e1535b09429b1e1171b88e6de2f1e0b4ec2be83
alexeymasnoy <masnoy0alexey@gmail.com>
2016-06-13 11:48:00 +0300

    svg

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Loftshool builder
+# Loftshool builder: First Ptoject Portfolio
 
 Getting started:
 
--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -4,24 +4,16 @@ module.exports = function () {
 	$.gulp.task('svg-sprite', function () {
 		return $.gulp.src('./source/images/*.svg')
 		.pipe($.gp.svgSprite({
-			shape: {
-				spacing: {
-					padding: 5
-				}
-			},
 			mode: {
-				css: {
-					sprite: '../images/sprite.svg',
-					bust: false,
+				symbol: {
+					dest: './',
+					sprite: '../source/images/sprite.svg',
 					render: {
 						scss: {
-							dest: '../style/common/_sprite.scss'
+							dest: '../source/style/common/_sprite.scss'
 						}
 					}
 				}
-			},
-			variables: {
-				mapname: 'icons'
 			}
 		}))
 		.pipe($.gulp.dest('./source/'))
